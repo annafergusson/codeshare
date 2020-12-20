cc183a372b5001b94fd5f47f2217476463f8f14d
Anna Fergusson <annafergussonnz@gmail.com>
2020-12-20 21:23:30 +1300

    Print df as kable

--- R/rmdtext.R
+++ R/rmdtext.R
@@ -1,11 +1,8 @@
-## This app requires OpenCPU 1.0.1 or higher !!!! 
-##
-
 #' @export
 rmdtext <- function(text){
   writeLines(text, con="input.Rmd");
   rmarkdown::render("input.Rmd", 
-         output_format = "html_fragment",
-         output_file ="output.html");
+                    output_format = rmarkdown::html_fragment(df_print = 'kable'),
+                    output_file ="output.html");
   invisible();
 }
